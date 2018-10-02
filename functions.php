26f87bb0869b37c41e358841634f79cf35ede74b
Keramot UL Islam <39233955+AbmSourav@users.noreply.github.com>
2018-10-02 11:34:29 +0600

    functions.php
    add_theme_support( 'title-tags' );  ->  add_theme_support( 'title-tag' );

--- functions.php
+++ functions.php
@@ -26,7 +26,7 @@ if ( site_url() == "http://meal.lwhh.com" ) {
 function meal_theme_setup() {
 	load_theme_textdomain( 'meal', get_template_directory() . "/languages" );
 	add_theme_support( 'post-thumbnails' );
-	add_theme_support( 'title-tags' );
+	add_theme_support( 'title-tag' );
 	add_theme_support( 'automatic-feed-links' );
 	add_theme_support( 'html5', array(
 		'search-form',
